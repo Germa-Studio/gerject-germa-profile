4bba356ebe4fea44360f2783c9576b1e830a4e2a
fillahaufi <fillahaufi140101@gmail.com>
2022-11-08 02:32:39 +0700

    feat: installed tailwindcss, daisyui, eslint, prettier

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,7 +1,25 @@
 export default function home() {
-    return(
-        <div>
-            <h1>ini page Home</h1>
-        </div>
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+            <div className="alert alert-info shadow-lg">
+                <div>
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="stroke-current flex-shrink-0 w-6 h-6">
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
+                    <span>New software update available.</span>
+                </div>
+            </div>
+        </>
     );
 }
+
+export let APP_VERSION = "v1.0.0";
